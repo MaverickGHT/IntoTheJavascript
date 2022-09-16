e6d86e699b2685194fb92e1b2cc334f7f65a7486
Gabriele Crea <cregg27@gmail.com>
2022-09-16 15:31:00 +0200

    Seconda funzione

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 for (let i=0; i<100; i++) {
     let min= 0;
     let max= 100;
@@ -6,6 +7,12 @@ for (let i=0; i<100; i++) {
     document.write(Math.round(random_num * random_value) + br);
     }
 
+//let Numero= prompt("Scrivi il numero di facce del dado");
+
+/* for (let i=0; i<100; i++) {
+    let random_value = Math.random();
+    document.write(Math.round(Numero * random_value) + br);
+    } */
 
 
 
