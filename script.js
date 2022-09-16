78583bb80f9b87e4e3f736d551c6a18f76dc59d5
Gabriele Crea <cregg27@gmail.com>
2022-09-16 12:43:15 +0200

    Aggiunta cicli

---
+++ script.js
@@ -0,0 +1,34 @@
+window.console.log("Ho fatto il c***o di caffè");
+document.write("Ho fatto il caffè");
+var Tazzina = 0;
+let Nuovatazzina = 0;
+const Caffe = 23; // questo è un bel commento simpatico 
+/* questo è un commento C like,
+così non vengo cazziato male.... o forse sì */
+let lattuga = 1;
+let cipolla = 0.989;
+let banane = 'Gianni';
+let zucchero = true;
+
+if (cipolla > 2) {
+    zucchero = false;
+} else {
+    cipolla++; // cipolla = cipolla + 1 
+}
+
+document.write(cipolla + "<br>");
+
+for (let i=0; i< 100; i++) {
+    lattuga++;
+}
+
+while (lattuga < 102 ) {
+    cipolla++;
+    lattuga++;
+}
+
+document.write(cipolla + "\n");
+
+let aglio = 1 + 3;
+
+
