87a7457ff06d320c4c01673e5dc321e17e05c91e
Gabriele Crea <cregg27@gmail.com>
2022-09-16 15:25:49 +0200

    Numero random dado

--- script.js
+++ script.js
@@ -1,42 +1,10 @@
-window.console.log("Ho fatto il c***o di caffè");
-var Tazzina = 0;
-let Nuovatazzina = 0;
-const Caffe = 23; // questo è un bel commento simpatico 
-/* questo è un commento C like,
-così non vengo cazziato male.... o forse sì */
-let lattuga = 1;
-let cipolla = 0.989;
-let banane = 'Gianni';
-let zucchero = true;
-const br = "<br>";
-
-document.write("Ho fatto il caffè" + br);
-
-if (cipolla > 2) {
-    zucchero = false;
-} else {
-    cipolla++; // cipolla = cipolla + 1 
-}
-
-document.write(cipolla + br);
-
-for (let i=0; i< 100; i++) {
-    lattuga++;
-}
-
-while (lattuga < 102 ) {
-    cipolla++;
-    lattuga++;
-}
-
-document.write(cipolla + br);
-
-let aglio = 1 + 3;
-let pomodoro = 2; 
-let peperone = 3;
-
-document.write(pomodoro ** peperone + br);
-document.write(br + br+ br+ br);
+for (let i=0; i<100; i++) {
+    let min= 0;
+    let max= 100;
+    let random_num = Math.random() * (max - min) + min;
+    let random_value = Math.random();
+    document.write(Math.round(random_num * random_value) + br);
+    }
 
 
 
