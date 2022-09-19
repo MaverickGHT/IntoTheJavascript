4af5c0bbae0f6b031257c96b36c46ece7f42af61
Gabriele Crea <cregg27@gmail.com>
2022-09-19 11:07:30 +0200

    Aggiunta del GitIgnore

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
+
+//const prompt = require('prompt-sync')();
+
 const br = "<br>";
-for (let i=0; i<100; i++) {
+/* for (let i=0; i<100; i++) {
     let min= 1;
     let max= 100;
     let random_num = Math.random() * (max - min) + min;
@@ -21,16 +24,23 @@ if (Numero > 0) {
     } 
 } else {
     window.alert('ERRORE SOLO NUMERO POSITIVO ACCETTATO');
+} */
+
+
+let Lista = [1,'1', 1.1];
+console.log(Lista);
+
+for (let i=0; i<=2; i++) {
+    console.log(Lista[i]);
 }
 
+/*while (1==1) {
+    console.log ("Aiuto!"); // se scrivi un while così sei proprio un demente  
+} */
 
-
-
-
-
-
-
-
+if (1==1) {
+    console.log("controllo che 1 sia uguale a 1");
+}
 
 
 
